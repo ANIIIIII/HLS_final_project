f48f6598106477cd848db0d00f80034731f1965f
Austin Chen <46265721+Oxtin@users.noreply.github.com>
2021-06-28 16:19:19 +0800

    Update host.py

--- src/host/host.py
+++ src/host/host.py
@@ -5,7 +5,7 @@ from pynq import allocate
 from __future__ import print_function
 
 ROOT = '/home/xilinx/IPBitFile'
-PATH = ROOT+'/design_1.bit'
+PATH = ROOT + '/design_1.bit'
 
 ol = Overlay(PATH)
 encIP = ol.encrypt_0
@@ -18,30 +18,48 @@ AP_IDLE = 0x04
 AP_READY = 0x08
 
 # i/O port
-KEY_V_1 = 0x10
-KEY_V_2 = 0x14
-KEY_V_3 = 0x18
-KEY_V_4 = 0x1C
+KEY_V = 0x10
+# KEY_V_2 = 0x14
+# KEY_V_3 = 0x18
+# KEY_V_4 = 0x1C
 
-P_V_I_1 = 0x38
-P_V_I_2 = 0x3C
+P_V_I = 0x24
+# P_V_I_2 = 0x28
 
-P_V_O_1 = 0x44
-P_V_O_2 = 0x48
+P_V_O_1 = 0x30
+P_V_O_2 = 0x34
 
-encIP.write(KEY_V_1, 0)
-encIP.write(KEY_V_2, 0)
-encIP.write(KEY_V_3, 0)
-encIP.write(KEY_V_4, 0)
+key = input("Enter 128-bit key (in hex form): ") or "0x0"
+plaintext = input("Enter 64-bit plaintext (in hex form): ") or "0x68656c6c6f030303"
 
-encIP.write(P_V_I_1, 0x6f030303)
-encIP.write(P_V_I_2, 0x68656c6c)
+print("key: ", key)
+print("plaintext: ", plaintext)
+
+key = int(key, 16)
+plaintext = int(plaintext, 16)
+flag = 0xffffffff
+
+# write key input
+for i in range(4):
+    encIP.write(KEY_V + 4*i, key & flag)
+    key = key >> 32
+
+# write plain-text input
+for i in range(2):
+    encIP.write(P_V_I + 4*i, plaintext & flag)
+    plaintext = plaintext >> 32
 
 # start
 encIP.write(CONTROL, AP_START)
+exeStartTime = time()
 while encIP.read(CONTROL) & AP_IDLE == 0x0:
 	continue
 
+exeEndTime = time()
 res_low = encIP.read(P_V_O_1)
 res_high = encIP.read(P_V_O_2)
-print(hex(res_high), hex(res_low))
+print("encypted text: ", hex(res_high) + hex(res_low)[2:])
+print("execute time: ", str(1000 * (exeEndTime - exeStartTime)), "ms")
+
+print("=====================================")
+print("process done!")
